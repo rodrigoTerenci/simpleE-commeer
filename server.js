e037645198985842b1676bf8e04d5fb1527550ac
rodrigoTerenci <rodrigoterenci@gmail.com>
2021-07-27 20:40:47 +0000

    test

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const
   server = http.createServer(app),
   port = process.env.PORT || 5000
 
-
+ app.get('/produtos',(req,res)=>
+    res.send("Hello Worldfd")
+  )
 
 app.use(express.static('.'))
 server.listen(port)
